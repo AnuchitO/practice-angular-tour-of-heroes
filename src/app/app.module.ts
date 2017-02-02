2af7b8ff65ae28fdb37f4b55117ad02c7ec03e21
Pead Nong <anuchit.prasertsang@gmail.com>
2017-02-02 13:41:22 +0700

    create dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
@@ -23,6 +25,9 @@ import { HeroesComponent } from './heroes.component';
       {
         path: 'heroes',
         component: HeroesComponent
+      },{
+        path: 'dashboard',
+        component: DashboardComponent
       }
     ])
   ],
