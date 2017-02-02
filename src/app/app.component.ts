63f477ae7b3dfc14c3ed1f4c995d4d4ca95b0272
Pead Nong <anuchit.prasertsang@gmail.com>
2017-02-02 11:39:54 +0700

    back to get heroes not delay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -79,7 +79,7 @@ export class AppComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero): void {
